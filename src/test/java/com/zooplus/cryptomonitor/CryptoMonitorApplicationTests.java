4c37a6c2b26c2ce2aa1420fa284d5735263ae3d3
chamilp <chamil.prabodha@syscolabs.com>
2022-05-19 22:14:05 +0530

    init project using Spring Boot and Gradle

---
+++ src/test/java/com/zooplus/cryptomonitor/CryptoMonitorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zooplus.cryptomonitor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CryptoMonitorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
